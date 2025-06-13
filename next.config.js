8d7be23cf009339cd375028be7026c91f3414310
yvelkuri-stu <yvelkuri@stu.calums.edu>
2025-06-12 20:16:37 -0700

    removed experimental options from nextjs config

--- next.config.js
+++ next.config.js
@@ -66,10 +66,6 @@ const withPWA = require("next-pwa")({
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
 };
 
 module.exports = withPWA(nextConfig);
